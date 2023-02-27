ef6f8b7106c2c577ca304f001a328bc944857dfd
skyeblu7 <skyeblu7@protonmail.com>
2023-02-26 23:44:46 -0600

    Delete aoi22_run1 directory

--- aoi22_run1/xcelium.d/run.lnx8664.21.03.d/.timestamp.ts
+++
@@ -1,5 +0,0 @@
-1677372402 /home/koepcke2/ece425.work/aoi22_run1/ihnl/cds0/netlist
-1677372402 /home/koepcke2/ece425.work/aoi22_run1/hdlFilesDir/cds_globals.v
-1139589189 /software/ncsu-cdk-1.6.0.beta/lib/NCSU_Analog_Parts/nmos/functional/verilog.v
-1139589175 /software/ncsu-cdk-1.6.0.beta/lib/NCSU_Analog_Parts/pmos/functional/verilog.v
-1677372402 /home/koepcke2/ece425.work/aoi22_run1/testfixture.template
