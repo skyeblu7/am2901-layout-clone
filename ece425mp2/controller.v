204c61f956ab91376256f600cab7f9081eeb9fc5
koepcke2 <koepcke2@illinois.edu>
2023-04-06 23:24:18 -0500

    merged conflicts

--- ece425mp2/controller.v
+++ ece425mp2/controller.v
@@ -11,7 +11,19 @@ module controller(
 	//,reg_wr      //add additiona signals for your design here
 
 	// ===== Added signal =====
+	// Q reg mux signals
 	, select_q_reg
+
+	// regfile mux signals
+	, reg_wr
+	, select_regfile
+
+	// ALU mux signals
+	, select_ALU_r
+	, select_ALU_s
+
+	//output mux signal
+	, select_y
 
 	// ALU signals
 	, inv_r
@@ -41,7 +53,17 @@ input q0_data, q3_data;
 //output reg_wr    //define additional I/Os for your design
 
 // ===== Added signal =====
-output select_q_reg[1:0];
+// Q reg mux signals
+output [1:0] select_q_reg;
+// regfile mux signals
+output reg_wr;
+output [1:0] select_regfile;
+// ALU mux signals
+output [1:0] select_ALU_r;
+output [1:0] select_ALU_s;
+
+//output mux signal
+output select_y;
 
 // ALU signals
 output inv_r;
@@ -94,12 +116,59 @@ assign not_sel_f1 = ~sel_f1;
 //end
 
 always @(*) begin
-	// select_q_reg[1:0]
+	// Q reg signals
+	// select_q_reg [1:0]
 	// 	2'd0: none, 2'd1: shift right, 2'd2: load F, 2'd3: shift left
 	if(shift_right & (~i[6]))		select_q_reg = 2'd1; //i8i7i6 = 100; Q/2 -> Q
 	else if(shift_left & (~i[6]))	select_q_reg = 2'd3; //i8i7i6 = 110; 2Q  -> Q
 	else if(i[8:6] == 3'd0)			select_q_reg = 2'd2; //i8i7i6 = 000; F   -> Q
 	else							select_q_reg = 2'd0; //else			 Q   -> Q
+
+	// regfile signals
+	// reg_wr
+	reg_wr = (~i[8]) & (~i[7]);
+	// select_regfile [1:0]
+	case (i[8:7])
+		2'd0: select_regfile = 2'd1; //default DONT CARE, reg_wr == 0
+		2'd1: select_regfile = 2'd1; //F   -> B
+		2'd2: select_regfile = 2'd0; //F/2 -> B
+		2'd3: select_regfile = 2'd2; //2F  -> B
+	endcase
+
+	// ALU mux signals
+	// select_ALU_r: 
+	// 2'd0: D
+	// 2'd1: A
+	// 2'd2: gnd (O)
+	case (i[2:0])
+		3'd0: select_ALU_r = 2'd1; 
+		3'd1: select_ALU_r = 2'd1; 
+		3'd2: select_ALU_r = 2'd2; 
+		3'd3: select_ALU_r = 2'd2; 
+		3'd4: select_ALU_r = 2'd2;
+		3'd5: select_ALU_r = 2'd0; 
+		3'd6: select_ALU_r = 2'd0; 
+		3'd7: select_ALU_r = 2'd0; 
+	endcase
+	// select_ALU_s: 
+	// 2'd0: A
+	// 2'd1: B
+	// 2'd2: Q
+	// 2'd3: gnd (O)
+	case (i[2:0])
+		3'd0: select_ALU_s = 2'd2; 
+		3'd1: select_ALU_s = 2'd1; 
+		3'd2: select_ALU_s = 2'd2; 
+		3'd3: select_ALU_s = 2'd1; 
+		3'd4: select_ALU_s = 2'd0;
+		3'd5: select_ALU_s = 2'd0; 
+		3'd6: select_ALU_s = 2'd2; 
+		3'd7: select_ALU_s = 2'd3; 
+	endcase
+
+	//select_y
+	if(i[8:6] == 3'b010) select_y = 1'b0;
+	else				 select_y = 1'b1;
 
 	
 end
